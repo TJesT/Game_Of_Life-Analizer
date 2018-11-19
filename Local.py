a16ad488a851a236758f9fdfa42c8c78b130ab5b
Gena Jidkiy <42508019+TJesT@users.noreply.github.com>
2018-11-19 20:12:03 +0700

    Added examples

--- Local.py
+++ Local.py
@@ -4,3 +4,6 @@ RU_INPUT_FIELD_SIZE = "Введите размер поля"
 RU_INPUT_FIELD = "Введите начальное состояние"
 
 RU_ERR_FIELD_INPUT = "Неверно введено поле или его размеры"
+
+EXAMPLE_STATE_FOR_YES = "xxxxx\nxx*xx\n***xx\nxx*xx\nxxxxx"
+EXAMPLE_STATE_FOR_NO = "*xxxx\nx*xxx\nx**xx\nxxxxx\nxxxxx"
